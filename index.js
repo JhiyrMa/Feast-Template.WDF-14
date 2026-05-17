d5a4649b663ff38d11eba36e132aff7199f19931
Kyaw Min Thant <kt090575@gmail.com>
2026-05-17 15:49:55 +0630

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const reviewers = document.querySelectorAll(".reviewer");
+const reviewText = document.getElementById("review");
+
+const reviews = [
+    "The cozy atmosphere immediately made us feel at home, & every detail of the ambiance added to the overall experience. Each dish was prepared with the freshest ingredients and burst with incredible flavors. The staff was attentive, friendly, and made sure we were taken care of throughout the meal.",
+    "The warm, inviting ambiance was the perfect setting for a relaxed dinner. The food was delicious, but I'd love to see more variety in the drink menu to match the fantastic food options. The service was friendly, attentive, and made us feel special. I'll definitely return to experience more of the menu!",
+    "Absolutely fantastic! The food was out of this world, made with the freshest ingredients & bursting with flavor. The staff was attentive and knowledgeable, offering great recommendations. I especially loved the chef's specials, which were beautifully presented and full of creative flavors."
+];
+
+reviewers.forEach((reviewer, index) => {
+
+    reviewer.addEventListener("click", () => {
+        reviewers.forEach(i => {
+            i.classList.remove("active");
+        });
+
+        reviewer.classList.add("active");
+
+        reviewText.classList.add("changing");
+        setTimeout(() => {
+            reviewText.textContent = reviews[index];
+            reviewText.classList.remove("changing");
+        }, 400);
+
+    });
+
+});
+
+const faqItems = document.querySelectorAll(".faq-q-and-a");
+
+faqItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        faqItems.forEach((faq) => {
+            if(faq !== item){
+                faq.classList.remove("active");
+            }
+        })
+        item.classList.toggle("active");
+    })
+})
